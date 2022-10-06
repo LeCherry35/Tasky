d4afe6d43ee1061f1f0388865e63ca245c9973f7
Cherry <lenyamm35@gmail.com>
2022-10-06 11:24:36 +0300

    drag and drop fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,46 @@
 import React from 'react';
 import { useState } from 'react';
-import { Todo } from './types/model'
+import { Todo } from './models/Todo'
 import './App.css';
 import InputField from './components/InputField';
 import TodoList from './components/TodoList';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd'
 import ClearButton from './components/ClearButton';
+import { useTypedSelector } from './hooks/useTypedSelector';
+import LoginForm from './components/LoginForm';
+import { useDispatch } from 'react-redux';
 
 const App: React.FC = () => {
 
 
 
-  const initTodosJSON: string | null = localStorage.getItem('todos')
-  const initTodos: Todo[] = initTodosJSON ? JSON.parse(initTodosJSON) : []
-  const initCompletedTodosJSON: string | null = localStorage.getItem('completedTodos')
-  const initCompletedTodos: Todo[] = initCompletedTodosJSON ? JSON.parse(initCompletedTodosJSON) : []
+  // const initTodosJSON: string | null = localStorage.getItem('todos')
+  // const initTodos: Todo[] = initTodosJSON ? JSON.parse(initTodosJSON) : []
+  // const initCompletedTodosJSON: string | null = localStorage.getItem('completedTodos')
+  // const initCompletedTodos: Todo[] = initCompletedTodosJSON ? JSON.parse(initCompletedTodosJSON) : []
 
 
  
-  const [todo, setTodo] = useState<string>('')
-  const [todos, setTodos] = useState<Todo[]>(initTodos)
-  const [completedTodos, setCompletedTodos] = useState<Todo[]>(initCompletedTodos)
+  // const [todo, setTodo] = useState<string>('')
+  // const [todos, setTodos] = useState<Todo[]>(initTodos)
+  // const [completedTodos, setCompletedTodos] = useState<Todo[]>(initCompletedTodos)
 
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault()
-    if(todo) {
-      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
-      setTodo('')
-      
-    }
-  }
+  const {todos, completedTodos} = useTypedSelector(state => state.todos)
+const dispatch = useDispatch()
 
   const onDragEnd = (result:DropResult) => {
-    const {source, destination} = result
-    if (!destination) return
-    if (destination.droppableId === source.droppableId && destination.index === source.index) return
-    let add, 
-      active = todos,
-      completed = completedTodos
-    
-    if (source.droppableId === 'TodosList') {
-      add = active[source.index]
-      active.splice(source.index, 1)
-    } else {
-      add = completed[source.index]
-      completed.splice(source.index, 1)
-    }
-    
-    if (destination.droppableId === 'TodosList') {
-      add.isDone = false
-      active.splice(destination.index, 0, add)
-    } else {
-      add.isDone = true
-      completed.splice(destination.index, 0, add)
-    }
-    setCompletedTodos(completed)
-    setTodos(active)
+    dispatch({type: 'DRAG_END', payload: result})
   }
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
+        <LoginForm />
         <span className="heading">Tasky</span>
         <InputField />
         <TodoList />
-        { todos.length !== 0 || completedTodos.length !== 0 
+        {/* { todos.length !== 0 || completedTodos.length !== 0 
           ? <ClearButton setTodos={setTodos} setCompletedTodos={setCompletedTodos}/>
-          : <></>}
+          : <></>} */}
       </div>
 
     </DragDropContext>
--- src/store/reducers/todoReducer.ts
+++ src/store/reducers/todoReducer.ts
@@ -1,4 +1,4 @@
-import { Todo } from '../../types/model'
+import { Todo } from '../../models/Todo'
 import _ from 'lodash'
 
 const ADD_TODO = 'ADD_TODO'
@@ -6,6 +6,7 @@ const EDIT_TODO = 'EDIT_TODO'
 const REMOVE_TODO = 'REMOVE_TODO'
 const SET_DONE = 'SET_DONE'
 const SET_UNDONE = 'SET_UNDONE'
+const DRAG_END = 'DRAG_END'
 
 
 // const initTodosJSON: string | null = localStorage.getItem('todos')
@@ -23,11 +24,6 @@ interface TodosAction {
     type: string
     payload?: any
 }
-
-// interface AddTodoAction {
-//     type:'ADD_TODO'
-//     payload: any
-// }
 
 const initialState: TodosState = {
     todos: [],
@@ -73,6 +69,31 @@ export const todoReducer = (state = initialState, action: TodosAction): TodosSta
             }
             return { ...state, todos: updatedTodos, completedTodos: leftCompletedTodos}
 
+        case DRAG_END:
+            const {source, destination} = action.payload
+            if (!destination) return state
+            if (destination.droppableId === source.droppableId && destination.index === source.index) return state
+
+            let add, 
+            active = _.cloneDeep(state.todos),
+            completed = _.cloneDeep(state.completedTodos)
+            
+            if (source.droppableId === 'TodosList') {
+                add = active[source.index]
+                active.splice(source.index, 1)
+            } else {
+                add = completed[source.index]
+                completed.splice(source.index, 1)
+            }
+            
+            if (destination.droppableId === 'TodosList') {
+                add.isDone = false
+                active.splice(destination.index, 0, add)
+            } else {
+                add.isDone = true
+                completed.splice(destination.index, 0, add)
+            }
+            return { ...state, todos: active, completedTodos: completed}
         default:
             return state
     }
