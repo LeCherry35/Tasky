e6ee5b13420a55e4c604331272adafc6ccc69bf3
Cherry <lenyamm35@gmail.com>
2022-12-09 13:33:47 +0200

    auth panel reducer deleted

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,11 +1,9 @@
-import { authPanelReducer } from './authPanelReducer';
 import {combineReducers} from 'redux'
 import { todoReducer } from './todoReducer'
 import { userReducer } from './userReducer'
 
 export const rootReducer = combineReducers({
     todos: todoReducer,
-    user: userReducer,
-    authPanel: authPanelReducer
+    user: userReducer
 })
 
